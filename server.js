9934fd2f83222e1ce1f86275e5c3715397ef52f6
sergeynnn <nifontov1993@bk.ru>
2018-02-15 00:34:42 +0300

    fix

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var bot = new TelegramBot(token, {polling: true});
 var fs = require('fs')
 var MongoClient = require("mongodb").MongoClient;
 
+url = 'mongodb://telegram_bot:ewr3377nd@ds235388.mlab.com:35388/telegram_bot';
+mongoClient.connect(url, function(err, db){
+    var collection = db.collection("message-logs");
+    var user = {name: "Tom", age: 23};
+    collection.insertOne(user, function(err, result){
+         
+        if(err){ 
+            return console.log(err);
+        }
+        console.log(result.ops);
+        db.close();
+    });	
+});
+
 bot.on('message', function(msg) {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Received your message');
