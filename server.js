45d3286ca6f3da13107fd7a0d65d09dfc70935ac
sergeynnn <nifontov1993@bk.ru>
2018-02-14 22:08:44 +0300

    add bot functionality

--- server.js
+++ server.js
@@ -2,19 +2,13 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '541428253:AAEQXJyWUkj79-hZzWMe4QYUk3n6OHxw6lQ'; 
 var bot = new TelegramBot(token, {polling: true});
 
-bot.onText(//echo (.+)/, function (msg, match) {
+bot.onText( function (msg, match) {
     var fromId = msg.from.id;
     var resp = match[1];
     bot.sendMessage(fromId, resp);
 });
 
-// Простая команда без параметров.
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
-    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-    var photo = '1069078_original.jpg';
-    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
-});
+
 
 var http = require('http'),
     port = process.env.PORT || 8001;
