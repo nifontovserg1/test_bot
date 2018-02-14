6868248aad3ee96e95cc8d11355778c6419de778
sergeynnn <nifontov1993@bk.ru>
2018-02-14 21:18:15 +0300

    add telegram bot

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var telegramBot = require('node-telegram-bot-api');
+
 var http = require('http'),
     port = process.env.PORT || 8001;
  
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end("I'm a telegram bot \n");
 }).listen(parseInt(port));
