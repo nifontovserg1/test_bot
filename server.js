ec0048780711fcf383b16a8c0311ceb5a2ed6b54
sergeynnn <nifontov1993@bk.ru>
2018-02-14 21:22:51 +0300

    add bot functionality

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 var telegramBot = require('node-telegram-bot-api');
+var token = '541428253:AAEQXJyWUkj79-hZzWMe4QYUk3n6OHxw6lQ', bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+	
+	bot.sendMessage(userId, text+' '+time);
+});
+
 
 var http = require('http'),
     port = process.env.PORT || 8001;
