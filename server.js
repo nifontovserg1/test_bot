876299fc82a0b5b9af05257c19126ff10ce09bc2
sergeynnn <nifontov1993@bk.ru>
2018-02-15 00:49:46 +0300

    fix

--- server.js
+++ server.js
@@ -7,16 +7,7 @@ var MongoClient = require("mongodb").MongoClient;
 url = 'mongodb://telegram-bot:ewr3377nd@ds235388.mlab.com:35388/telegram_bot';
 mongoClient.connect(url, function(err, db){
      
-    var collection = db.collection("message-logs");
-    var user = {name: "Tom", age: 23};
-    collection.insertOne(user, function(err, result){
-         
-        if(err){ 
-            return console.log(err);
-        }
-        console.log(result.ops);
-        db.close();
-    });
+
 });
 
 bot.on('message', function(msg) {
