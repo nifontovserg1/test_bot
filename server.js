61f581316d526ff45c3d7a15e07870642d42e600
sergeynnn <nifontov1993@bk.ru>
2018-02-14 23:31:13 +0300

    add bot functionality

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var fs = require('fs')
 
 bot.on('message', function(msg) {
   const chatId = msg.chat.id;
-  fs.writeFileSync('params.json', 'a' , 'utf-8');
+  fs.writeFileSync('params.log', 'a' , 'utf-8');
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
 });
