b2628e0e9aa916a5be171d6b94a5121f78889ca5
sergeynnn <nifontov1993@bk.ru>
2018-02-15 00:40:32 +0300

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '541428253:AAEQXJyWUkj79-hZzWMe4QYUk3n6OHxw6lQ'; 
 var bot = new TelegramBot(token, {polling: true});
 
-
+var MongoClient = require("mongodb").MongoClient;
 
 
 
