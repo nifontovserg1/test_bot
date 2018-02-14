7185654aec9b438e09354f8212d87a1e3673f88d
sergeynnn <nifontov1993@bk.ru>
2018-02-14 21:50:07 +0300

    fix

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var telegramBot = require('node-telegram-bot-api');
+var TelegramBot = require('node-telegram-bot-api');
 var token = '541428253:AAEQXJyWUkj79-hZzWMe4QYUk3n6OHxw6lQ'; 
 var bot = new TelegramBot(token, {polling: true});
 
