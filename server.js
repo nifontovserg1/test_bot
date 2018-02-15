b89bedb6548955c767c0329e760ad9e4a8a25be0
sergeynnn <nifontov1993@bk.ru>
2018-02-15 17:39:06 +0300

    time format

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ var bot = new TelegramBot(token, {polling: true});
 function timeConverter(UNIX_timestamp) {
 	var a = new Date(UNIX_timestamp * 1000);
 	  var a = new Date(UNIX_timestamp * 1000);
-	  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	 
 	  var year = a.getFullYear();
-	  var month = months[a.getMonth()];
+	  var month =a.getMonth();
 	  var date = a.getDate();
 	  var hour = a.getHours();
 	  var min = a.getMinutes();
 	  var sec = a.getSeconds();
-	  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+	  var time = date + '.' + month + '.' + year + ' ' + hour + ':' + min + ':' + sec ;
 	  return time;	
 }
 
