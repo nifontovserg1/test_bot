03d15394912be383010b3a8a705760abadbea7ba
sergeynnn <nifontov1993@bk.ru>
2018-02-15 22:40:43 +0300

    aa

--- server.js
+++ server.js
@@ -65,11 +65,11 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("Hello, I'm a telegram bot <br>");
   mongo.connect('mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl',  function(error, db){
-	res.write('connected');
+
 	var collection = db.db('heroku_2l11m0jl').collection('messages');
 	collection.find().toArray(function(err, results) {
 
-		res.write('d');
+		res.write(results.length);
 	});
 	db.close();
   });
