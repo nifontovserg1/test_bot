34262d2723e246ad54772091716f35db99cb6601
sergeynnn <nifontov1993@bk.ru>
2018-02-15 14:46:12 +0300

    aa

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ var http = require('http'),
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   
-mongo.connect('mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl',  function(error, db){
+mongo.connect('mongodb://admin1:admin@ds235778.mlab.com:35778/heroku_2l11m0jl',  function(error, db){
 	res.write(error+'e');
+	
 });
   
   res.end("I'm a telegram bot \n");
