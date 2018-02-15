9d8671a5055b70a6dca5644cc180c1725503d824
sergeynnn <nifontov1993@bk.ru>
2018-02-15 13:00:47 +0300

    db test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var token = '541428253:AAEQXJyWUkj79-hZzWMe4QYUk3n6OHxw6lQ';
 var bot = new TelegramBot(token, {polling: true});
 //var fs = require('fs')
 var MongoClient = require("mongodb").MongoClient;
-var url = 'mongodb://admin:admin@ds235778.mlab.com:35778';
+var url = 'mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl';
 
 
 
@@ -24,7 +24,7 @@ http.createServer(function (req, res) {
 	if(err) {
 		res.write('error');
 	} else {
-		
+	
 		
 		db.close();
 	}
