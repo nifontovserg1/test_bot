1b5f5eaac5d870b17a0272d217dbd86c16375a05
sergeynnn <nifontov1993@bk.ru>
2018-02-15 12:17:01 +0300

    db test

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var bot = new TelegramBot(token, {polling: true});
 //var fs = require('fs')
 var MongoClient = require("mongodb").MongoClient;
 var url = 'mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl';
+  MongoClient.connect(url, function(db, er) {
+	var collection = db.collection("messages");
+
+  })  
 
 
 
@@ -18,18 +22,6 @@ var http = require('http'),
  
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  MongoClient.connect(url, function(db, er) {
-	var collection = db.collection("messages");
-	var user = {name: "Tom", age: 23};
-	collection.insertOne(user, function(err, result){
-		 
-		if(err){ 
-			res.write(err);
-		}
-		res.write(result.ops);
-		db.close();
-	});
-  })  
 
   res.end("I'm a telegram bot \n");
 }).listen(parseInt(port));
