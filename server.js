6b064d461e6924fa937e2b2b9529b9f177c2817c
sergeynnn <nifontov1993@bk.ru>
2018-02-15 14:21:45 +0300

    db test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var token = '541428253:AAEQXJyWUkj79-hZzWMe4QYUk3n6OHxw6lQ';
 var bot = new TelegramBot(token, {polling: true});
 //var fs = require('fs')
 
-var url = 'mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl';
+var url = 'mongodb://admin1:admin@ds235778.mlab.com:35778/heroku_2l11m0jl';
 
 var connect = require('connect'),
   mongo = require('mongodb').MongoClient;
@@ -23,11 +23,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   
 mongo.connect(url, {}, function(error, db){
-	if(error) {
-		res.write(error);
-	} else {
-		var col =  db.collection('messages');
-	}
+
 });
   
   res.end("I'm a telegram bot \n");
