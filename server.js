da04a3f40885f3d9f08f844bdd0940b000bc8ca8
sergeynnn <nifontov1993@bk.ru>
2018-02-15 22:39:36 +0300

    add table

--- server.js
+++ server.js
@@ -66,10 +66,9 @@ http.createServer(function (req, res) {
   res.write("Hello, I'm a telegram bot <br>");
   mongo.connect('mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl',  function(error, db){
 	res.write('connected');
-	res.write(error);
 	var collection = db.db('heroku_2l11m0jl').collection('messages');
 	collection.find().toArray(function(err, results) {
-		res.write(err);
+
 		res.write('d');
 	});
 	db.close();
