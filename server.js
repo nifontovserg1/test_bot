f8b0504e0a5e91bb049b0289a873c4428d1f5886
sergeynnn <nifontov1993@bk.ru>
2018-02-15 12:01:28 +0300

    db test

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ var token = '541428253:AAEQXJyWUkj79-hZzWMe4QYUk3n6OHxw6lQ';
 var bot = new TelegramBot(token, {polling: true});
 //var fs = require('fs')
 var MongoClient = require("mongodb").MongoClient;
+var url = 'mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl';
+
+MongoClient.connect(url, function(db, er) {
+});
 
 bot.on('message', function(msg) {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Received your message');
 });
-
 
 
 var http = require('http'),
