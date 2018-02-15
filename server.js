8a338bf8658bdc2e0d825c1f3c3ee7405667746a
sergeynnn <nifontov1993@bk.ru>
2018-02-15 20:48:53 +0300

    aa

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ var http = require('http'),
 	
  
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("I'm a telegram bot \n");
   res.end();
 }).listen(parseInt(port));
