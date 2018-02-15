a9b2265062d6d1694ef18a3f42d5398faec6f609
sergeynnn <nifontov1993@bk.ru>
2018-02-15 14:12:40 +0300

    db test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '541428253:AAEQXJyWUkj79-hZzWMe4QYUk3n6OHxw6lQ'; 
 var bot = new TelegramBot(token, {polling: true});
 //var fs = require('fs')
-var MongoClient = require("mongodb").MongoClient;
+
 var url = 'mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl';
 
 var connect = require('connect'),
