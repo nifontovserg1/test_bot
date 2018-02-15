e26fb0ce6b4fbba3a9db51eddf1b36db1b9b199c
sergeynnn <nifontov1993@bk.ru>
2018-02-15 17:36:56 +0300

    add time conv

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ var bot = new TelegramBot(token, {polling: true});
 
  var mongo = require('mongodb').MongoClient;
 
+function timeConverter(UNIX_timestamp) {
+	var a = new Date(UNIX_timestamp * 1000);
+	  var a = new Date(UNIX_timestamp * 1000);
+	  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	  var year = a.getFullYear();
+	  var month = months[a.getMonth()];
+	  var date = a.getDate();
+	  var hour = a.getHours();
+	  var min = a.getMinutes();
+	  var sec = a.getSeconds();
+	  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+	  return time;	
+}
 
-
-bot.on('message', function(msg) {
-  const chatId = msg.from.id, date = msg.date;
-  bot.sendMessage(chatId, 'Received your message: '+date);
-});
-
-
-var http = require('http'),
-    port = process.env.PORT || 8001;
- 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  
-mongo.connect('mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl',  function(error, db){
+/*mongo.connect('mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl',  function(error, db){
 	if(error) {
 		res.write(error+'\n');
 	} else {
@@ -33,7 +33,18 @@ mongo.connect('mongodb://admin:admin@ds235778.mlab.com:35778/heroku_2l11m0jl',  
 				db.close();
 		});		
 	}
+});*/
+
+bot.on('message', function(msg) {
+  const chatId = msg.from.id, date = msg.date;
+  bot.sendMessage(chatId, 'Received your message: '+timeConverter(date));
 });
-  
+
+
+var http = require('http'),
+    port = process.env.PORT || 8001;
+ 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("I'm a telegram bot \n");
 }).listen(parseInt(port));
