1e397633eae4d35f7541e5043cf8fb330b28be54
sergeynnn <nifontov1993@bk.ru>
2018-02-15 17:40:44 +0300

    aa

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ function timeConverter(UNIX_timestamp) {
 	  var a = new Date(UNIX_timestamp * 1000);
 	 
 	  var year = a.getFullYear();
-	  var month =a.getMonth();
+	  var month =a.getMonth()+1;
+	  if(month<10) {
+		  month = '0'+month;
+	  }
 	  var date = a.getDate();
 	  var hour = a.getHours();
 	  var min = a.getMinutes();
